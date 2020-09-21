992fb969234233dc043f405f8e8fd97770961ec2
Daniel Soares <>
2020-09-21 06:03:26 -0300

    criei uma table view sem cell prototype para treinar os passos de criação de table views

---
+++ outraTableView/outraTableView/ViewController.swift
@@ -0,0 +1,36 @@
+//
+//  ViewController.swift
+//  outraTableView
+//
+//  Created by Daniel Soares on 21/09/20.
+//  Copyright © 2020 Daniel Soares. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    var tableViewData = ["one", "two", "three", "four", "five", "six"]
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.tableView.delegate = self
+        self.tableView.dataSource = self
+        // Do any additional setup after loading the view.
+    }
+
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.tableViewData.count
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = UITableViewCell()
+        cell.textLabel?.text = self.tableViewData[indexPath.row]
+        return cell
+    }
+}
